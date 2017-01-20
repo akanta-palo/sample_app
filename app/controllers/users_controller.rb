b4d7939121da2ee6426f2d2e52978b29e41e6d10
PALO IT-Andrian <paloit-andrian@Palo-It-Macboook.singnet.com.sg>
2017-01-20 10:31:28 +0800

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the promise land!"
       redirect_to @user
     else
       render 'new'
     end
+  end
+  def destroy
   end
 
   private
